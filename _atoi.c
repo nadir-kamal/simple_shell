400c04462522ca487a6497faf05ce172c87d5384
NadirKamal <Kamal01nadir@gmail.com>
2023-05-24 23:42:12 +0100

    code project simple shell

---
+++ _atoi.c
@@ -0,0 +1,67 @@
+#include "shell.h"
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: the struct address
+ * Return: if interactive mode 1, 0 otherwise
+ */
+int interactive(ino_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: a char to check
+ * @delim: a delimeter string
+ * Return: true 1, false 0
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character to input
+ * Return: if c is alphabetic 1, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+/**
+ * _atoi - converts the string to an integer
+ * @s: string to be converted
+ * Return: if no numbers in string 0, converted number otherwise
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
