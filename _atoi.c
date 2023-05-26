69a0807c9c64ae5a211774af19b1ad4376d922bb
NadirKamal <Kamal01nadir@gmail.com>
2023-05-26 23:23:17 +0100

    atoi _c

--- _atoi.c
+++ _atoi.c
@@ -1,18 +1,21 @@
 #include "shell.h"
+
 /**
  * interactive - returns true if shell is interactive mode
- * @info: the struct address
- * Return: if interactive mode 1, 0 otherwise
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
  */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
+
 /**
  * is_delim - checks if character is a delimeter
- * @c: a char to check
- * @delim: a delimeter string
- * Return: true 1, false 0
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
  */
 int is_delim(char c, char *delim)
 {
@@ -21,11 +24,13 @@ int is_delim(char c, char *delim)
 			return (1);
 	return (0);
 }
+
 /**
  * _isalpha - checks for alphabetic character
- * @c: character to input
- * Return: if c is alphabetic 1, 0 otherwise
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -33,11 +38,13 @@ int _isalpha(int c)
 	else
 		return (0);
 }
+
 /**
- * _atoi - converts the string to an integer
- * @s: string to be converted
- * Return: if no numbers in string 0, converted number otherwise
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
+
 int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
@@ -65,3 +72,4 @@ int _atoi(char *s)
 
 	return (output);
 }
+
